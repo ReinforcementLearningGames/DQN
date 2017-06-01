d468d670e424e2515423c807aef7bc01db39c4ad
Frank Pham <fhpham@cpp.edu>
2017-06-01 12:50:16 -0700

    Final version of code

--- dqn_cartpole.py
+++ dqn_cartpole.py
@@ -10,78 +10,87 @@ from keras.layers import Dense, Activation, Flatten
 from keras.optimizers import Adam
 
 from rl.agents.dqn import DQNAgent
-from rl.policy import BoltzmannQPolicy
 from rl.memory import SequentialMemory
-from rl.callbacks import FileLogger, ModelIntervalCheckpoint
+from rl.policy import BoltzmannQPolicy
+from rl.callbacks import ModelIntervalCheckpoint
 
-ENV_NAME = 'CartPole-v0'
+ENV = 'CartPole-v0'
 
 def main():
+    # use parser to get command arguments to make it easier to change parameters
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', type=str, default='train', 
-                        choices=('train', 'test'))
-    parser.add_argument('--weights-filename', type=str, default=None)
-    parser.add_argument('--agent-memory-limit', type=int, default=50000)
-    parser.add_argument('--agent-warmup-steps', type=int, default=10)
+    parser.add_argument('--mode', type=str, default='train',
+                        choices=['train', 'test'])
+    parser.add_argument('--seed', type=int, default=None)
+    parser.add_argument('--memory_limit', type=int, default=40000)
+    parser.add_argument('--disable-ddqn', action='store_false')
     parser.add_argument('--target-model-update', type=int, default=0.01)
-    parser.add_argument('--disable-double-dqn', action='store_false')
-    parser.add_argument('--enable-dueling-network', action='store_true')
+    parser.add_argument('--discount-factor', type=float, default=0.99)
+    parser.add_argument('--warmup', type=int, default=10000)
     parser.add_argument('--lr', type=float, default=0.001)
-    parser.add_argument('--training-steps', type=int, default=50000)
+    parser.add_argument('--training-steps', type=int, default=40000)
+    parser.add_argument('--weights', type=str, default=None)
     parser.add_argument('--test-episodes', type=int, default=10)
-    parser.add_argument('--render-test', action='store_true')
+    parser.add_argument('--render', action='store_true')
     args = parser.parse_args()
 
-    # Create the environment
-    env = gym.make(ENV_NAME)
-    np.random.seed(123)
-    env.seed(123)
-    # Extract the number of actions from environment
+    # create new environment, seed environment, and get actions from environment
+    env = gym.make(ENV)
+    if args.seed is not None:
+        env.seed(args.seed)
     nb_actions = env.action_space.n
 
+    # build model
     model = Sequential()
-    model.add(Flatten(input_shape=(1,) + env.observation_space.shape))
-    model.add(Dense(16))
+    model.add(Flatten(input_shape=(1, ) + env.observation_space.shape))
+    model.add(Dense(32))
     model.add(Activation('relu'))
-    model.add(Dense(16))
+    model.add(Dense(32))
     model.add(Activation('relu'))
-    model.add(Dense(16))
+    model.add(Dense(32))
     model.add(Activation('relu'))
     model.add(Dense(nb_actions))
     model.add(Activation('linear'))
+    print(model.summary())
 
-    # Build DQN agent
-    memory = SequentialMemory(limit=args.agent_memory_limit, window_length=1)
+    # make experience memory of size
+    memory = SequentialMemory(limit=args.memory_limit, window_length=1)
+    # pick a simple policy for cartpole
     policy = BoltzmannQPolicy()
+
+    # create the DQN agent
     dqn = DQNAgent(
         model=model,
-        nb_actions=nb_actions,
-        memory=memory,
-        nb_steps_warmup=args.agent_warmup_steps,
-        target_model_update=args.target_model_update,
         policy=policy,
-        enable_double_dqn=args.disable_double_dqn,
-        enable_dueling_network=args.enable_dueling_network)
+        enable_double_dqn=args.disable_ddqn,
+        memory=memory,
+        target_model_update=args.target_model_update,
+        nb_steps_warmup=args.warmup,
+        gamma=args.discount_factor,
+        nb_actions=nb_actions
+    )
     dqn.compile(Adam(lr=args.lr), metrics=['mae'])
 
-    weights_filename = 'dqn_{}_weights.h5f'.format(ENV_NAME)
-    if args.weights_filename is not None:
-        weights_filename = args.weights_filename
+    weights_filename = 'cartpole_weights.h5f'
 
     if args.mode == 'train':
-        checkpoint_weights_filename = 'dqn_' + ENV_NAME + '_weights_{step}.h5f'
-        log_filename = 'dqn_{}_log.json'.format(ENV_NAME)
+        # make filenames for saving weights at checkpoints
+        checkpoint_weights_filename = 'cartpole_weights_{step}.h5f'
+        # add some callback to see reward and loss
         callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=10000)]
-        callbacks += [FileLogger(log_filename, interval=100)]
-        
+        # fit model for 40k steps
         dqn.fit(env, nb_steps=args.training_steps, callbacks=callbacks)
-
+        # Save final weights
         dqn.save_weights(weights_filename, overwrite=True)
-
-        dqn.test(env, nb_episodes=args.test_episodes, visualize=args.render_test)
     elif args.mode == 'test':
-        dqn.load_weights(args.weights_filename)
-        dqn.test(env, nb_episodes=args.test_episodes, visualize=args.render_test)
+        # Check if custom weights file. If so, assign new weights filename
+        if args.weights is not None:
+            weights_filename = args.weights
+        # Load weights from file
+        dqn.load_weights(weights_filename)
+        # Test model
+        dqn.test(env, nb_episodes=args.test_episodes, visualize=args.render)
+
 
 if __name__ == '__main__':
     main()
--- dqn_doom.py
+++ dqn_doom.py
@@ -1,28 +1,27 @@
-from __future__ import division
-from __future__ import print_function
 import argparse
-
-from PIL import Image
-
 import numpy as np
-import gym
+from PIL import Image
 
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Flatten, Permute
 from keras.layers.convolutional import Conv2D
+from keras.layers.normalization import BatchNormalization
 from keras.optimizers import Adam
 
-from rl.core import Processor
+import gym
+import ppaquette_gym_doom
+from ppaquette_gym_doom.wrappers import SetResolution, ToDiscrete
+
 from rl.agents.dqn import DQNAgent
-from rl.memory import SequentialMemory
 from rl.policy import LinearAnnealedPolicy, EpsGreedyQPolicy
+from rl.memory import SequentialMemory
+from rl.core import Processor
 from rl.callbacks import FileLogger, ModelIntervalCheckpoint
 
-
 OBSERVATION_SHAPE = (84, 84)
-ENV = 'Breakout-v0'
+ENV = 'ppaquette/DoomBasic-v0'
 
-class BreakoutProcessor(Processor):
+class DoomProcessor(Processor):
     def process_observation(self, observation):
         # convert from numpy array to image object
         img = Image.fromarray(observation)
@@ -39,30 +38,27 @@ class BreakoutProcessor(Processor):
         # change back to float and scale between 0.0 and 1.0
         return batch.astype('float32') / 255.0
 
-    def process_reward(self, reward):
-        # clip rewards to between -1 and 1
-        # anything less than 0 becomes -1
-        # anything greater than 0 becomes 1
-        return np.clip(reward, -1.0, 1.0)
+# wrap doom environment in order to work with OpenAI Gym and get actions
+# resolution is arbitrary, will be resized later
+def wrap_doom(env, resolution="640x480", actions="minimal"):
+    return SetResolution(resolution)(ToDiscrete(actions)(env))
 
 def main():
-    # use parser to get command arguments to make it easier to change parameters
+    # Capture command line options to easily change parameters
     parser = argparse.ArgumentParser()
-    parser.add_argument('--mode', type=str, default='train',
-                        choices=['train', 'test'])
-    parser.add_argument('--seed', type=int, default=None)
-    parser.add_argument('--memory-limit', type=int, default=1000000)
+    parser.add_argument('--mode', type=str, default='train', choices=('train', 'test'))
+    parser.add_argument('--memory-limit', type=int, default=50000)
     parser.add_argument('--frames', type=int, default=4)
     parser.add_argument('--initial-exploration', type=float, default=1.0)
     parser.add_argument('--final-exploration', type=float, default=0.1)
-    parser.add_argument('--final-exploration-frame', type=int, default=1000000)
+    parser.add_argument('--final-exploration-frame', type=int, default=10000)
     parser.add_argument('--disable-ddqn', action='store_false')
-    parser.add_argument('--train-interval', type=int, default=4)
-    parser.add_argument('--target-model-update', type=int, default=10000)
+    parser.add_argument('--train-interval', type=int, default=2)
+    parser.add_argument('--target-model-update', type=int, default=1500)
     parser.add_argument('--discount-factor', type=float, default=0.99)
-    parser.add_argument('--warmup', type=int, default=50000)
-    parser.add_argument('--lr', type=float, default=0.00025)
-    parser.add_argument('--training-steps', type=int, default=50000000)
+    parser.add_argument('--warmup', type=int, default=5000)
+    parser.add_argument('--lr', type=float, default=0.001)
+    parser.add_argument('--training-steps', type=int, default=1000000)
     parser.add_argument('--weights', type=str, default=None)
     parser.add_argument('--test-episodes', type=int, default=10)
     parser.add_argument('--render', action='store_true')
@@ -70,36 +66,42 @@ def main():
 
     # create new environment, seed environment, and get actions from environment
     env = gym.make(ENV)
-    if args.seed is not None:
-        env.seed(args.seed)
+    # wrap doom ennvironment to be able to get the actions, and observations
+    env = wrap_doom(env)
     nb_actions = env.action_space.n
 
     input_shape = (args.frames, ) + OBSERVATION_SHAPE
 
-    # build model based on what is desribed in Mnih et al (2015)
+    # build model. Similar to Breakout model but with added batch normalization
     model = Sequential()
     # input shape = (84, 84, 4)
     model.add(Permute((2, 3, 1), input_shape=input_shape))
-    model.add(Conv2D(32, (8, 8), strides=(4, 4)))
+    model.add(Conv2D(32, (8, 8)))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
-    model.add(Conv2D(64, (4, 4), strides=(2, 2)))
+    model.add(Conv2D(32, (4, 4)))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
-    model.add(Conv2D(64, (3, 3), strides=(1, 1)))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
     model.add(Flatten())
     model.add(Dense(512))
+    model.add(BatchNormalization())
     model.add(Activation('relu'))
+    model.add(Dense(512))
+    model.add(BatchNormalization())
+    model.add(Activation('linear'))
     model.add(Dense(nb_actions))
+    model.add(BatchNormalization())
     model.add(Activation('linear'))
     print(model.summary())
 
-    # make experience memory of size 1000000, stack 4 frames into one
+    # make experience memory of size 50k, stack 4 frames into one
     memory = SequentialMemory(limit=args.memory_limit, window_length=args.frames)
-    # create processor object
-    processor = BreakoutProcessor()
-    # linearly anneal epsilon greedy policy over 1000000 steps
+    # linearly anneal epsilon greedy policy over 10k steps
     policy = LinearAnnealedPolicy(
-        EpsGreedyQPolicy(),
+        inner_policy=EpsGreedyQPolicy(),
         attr='eps',
         value_max=args.initial_exploration,
         value_min=args.final_exploration,
@@ -107,7 +109,10 @@ def main():
         nb_steps=args.final_exploration_frame
     )
 
-    # create the DQN agent with parameters from Mnih et al (2015)
+    # create processor object
+    processor = DoomProcessor()
+
+    # create DQN agent, parameters are scaled-down from breakout parameters
     dqn = DQNAgent(
         model=model,
         policy=policy,
@@ -121,30 +126,30 @@ def main():
         nb_actions=nb_actions,
         delta_clip=1.0
     )
-
-    # compile agent with Adam instead of RMSProp
+    # complie agent and model
     dqn.compile(Adam(lr=args.lr), metrics=['mae'])
 
     # default weights filename
-    weights_filename = 'breakout_weights.h5f'
+    weights_filename = 'doom_weights.h5f'
 
     if args.mode == 'train':
         # make filenames for saving weights at checkpoints
-        checkpoint_weights_filename = 'breakout_weights_{step}.h5f'
+        checkpoint_weights_filename = 'doom_weights_{step}.h5f'
         # add some callback to see reward and loss
         callbacks = [ModelIntervalCheckpoint(checkpoint_weights_filename, interval=250000)]
-        # fit model for 50M steps
+        # fit model for 1M steps
         dqn.fit(env, nb_steps=args.training_steps, callbacks=callbacks)
         # Save final weights
         dqn.save_weights(weights_filename, overwrite=True)
     elif args.mode == 'test':
-        # Check for custom weights file. If so, assign new weights filename
+        # Check if custom weights file. If so, assign new weights filename
         if args.weights is not None:
             weights_filename = args.weights
         # Load weights from file
         dqn.load_weights(weights_filename)
         # Test model
         dqn.test(env, nb_episodes=args.test_episodes, visualize=args.render)
+
 
 if __name__ == '__main__':
     main()
